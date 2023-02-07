25d526d8c7aa7b5e3b9000db8952ca89f6306e96
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-08 00:45:51 +0530

    Pending

---
+++ 1. Basics of Programming I/3. Lets solve some Patterns/9. Hollow Pyramid.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    k = 0
+    for j in range(2 * n - 1):
+        if (j < n - i - 1):
+            print(" ",end='')
+        elif (k < 2 * i + 1):
+            if (k == 0) or (k== 2 * i) or (i == n - 1):
+                print("*",end='')
+            else:
+                print(" ",end='')
+                k += 1
+        else:
+            print(" ",end='')
+    print()

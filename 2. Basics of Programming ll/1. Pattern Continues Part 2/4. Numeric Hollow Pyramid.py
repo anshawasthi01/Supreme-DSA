7fd1888ecb9b3b64d15261e07df3f134ba5f8554
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-08 18:02:33 +0530

    Explained in Next Class

--- 2. Basics of Programming ll/1. Pattern Continues Part 2/4. Numeric Hollow Pyramid.py
+++ 2. Basics of Programming ll/1. Pattern Continues Part 2/4. Numeric Hollow Pyramid.py
@@ -1,1 +1,23 @@
 # HW
+n = int(input())
+
+for r in range(n):
+    for c in range(n - r - 1): #spaces
+        print(" ",end='')
+
+    start = 1
+    for c in range(2 * r + 1):
+        if r == 0 or r == n-1: #first row or last row 
+            if c % 2 == 0: #even
+                print(start,end='')
+                start = start + 1
+            else:
+                print(" ",end='') #odd
+        else:
+            if c == 0:
+                print(1,end='') # first col
+            elif c == 2 * r + 1 - 1:
+                print(r + 1,end='')
+            else:
+                print(" ",end='')
+    print()

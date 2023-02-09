e55f2d99acb0df2ca4f7fde192d05cd7cf6b82fe
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-09 16:46:37 +0530

    Practice

---
+++ 2. Basics of Programming ll/2. Operators, Loops & Conditionals/Practice.py
@@ -0,0 +1,30 @@
+# //	Division (floor): divides the first operand by the second
+
+a, b = 9, 4
+print(a // b)
+
+# is not	x is not the same as y
+
+Bitwise Operators in Python
+# Examples of Bitwise operators
+a = 10
+b = 4
+
+# Print bitwise AND operation
+print(a & b)
+
+# Print bitwise OR operation
+print(a | b)
+
+# Print bitwise NOT operation
+print(~a)
+
+# print bitwise XOR operation
+print(a ^ b)
+
+# print bitwise right shift operation
+print(a >> 2)
+
+# print bitwise left shift operation
+print(a << 2)
+

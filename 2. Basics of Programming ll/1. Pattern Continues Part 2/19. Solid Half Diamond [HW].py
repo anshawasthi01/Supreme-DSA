68c9bdc047ed19a24298150c4c54409d2945c964
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-11 19:38:56 +0530

    Week 2 solution video

--- 2. Basics of Programming ll/1. Pattern Continues Part 2/19. Solid Half Diamond [HW].py
+++ 2. Basics of Programming ll/1. Pattern Continues Part 2/19. Solid Half Diamond [HW].py
@@ -3,10 +3,10 @@ n = int(input())
 for i in range(2*n-1):
     condition = 0
 
-    if i < n:
+    if i < n: # growing phase of Diamond
         condition = i 
 
-    else:
+    else:     # shrinking phase of Diamond
         condition = n - (i % n) - 2
 
     for j in range(condition + 1):

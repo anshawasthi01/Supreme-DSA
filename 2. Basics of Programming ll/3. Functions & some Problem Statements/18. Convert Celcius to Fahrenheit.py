afa15c4ba9e05fc4db08301c765652c51ffceb35
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-12 00:38:10 +0530

    leetcode Week 2 solution video

---
+++ 2. Basics of Programming ll/3. Functions & some Problem Statements/18. Convert Celcius to Fahrenheit.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/convert-the-temperature/
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+'''
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        k = celsius + 273.15
+        f = celsius * 1.80 + 32.00
+
+        return[k, f]

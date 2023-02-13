bdb38532ad85d2b0db194bb35ae5dff553b2d78d
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-13 20:40:32 +0530

    Binar to Decimal

---
+++ 2. Basics of Programming ll/5. Number System - Binary & Decimal/3. Binary to Decimal.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+def BinaryToDecimal(n):
+    decimal = 0
+    i = 0
+    while n:
+        bit = n % 10
+        i += 1
+        decimal = decimal + bit * (2 ** i)
+        n //= 10
+    return decimal
+
+print(BinaryToDecimal(n))

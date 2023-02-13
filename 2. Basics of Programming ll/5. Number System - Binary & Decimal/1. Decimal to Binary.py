4bb2aef93ca47ff4a83fb3b7957c58a377fa52d0
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-13 20:18:54 +0530

    Division Method

--- 2. Basics of Programming ll/5. Number System - Binary & Decimal/1. Decimal to Binary.py
+++ 2. Basics of Programming ll/5. Number System - Binary & Decimal/1. Decimal to Binary.py
@@ -1,10 +1,21 @@
 n = int(input())
 
+# def DecimalToBinaryMethod1(n):
+#     # Division method
+#     while n > 0:
+#         bit = n % 2
+#         print(bit)
+#         n = n // 2
+
 def DecimalToBinaryMethod1(n):
     # Division method
+    binaryno = 0
+    i = 0
     while n > 0:
         bit = n % 2
-        print(bit)
+        i += 1
+        binaryno = bit * (10 ** i) + binaryno
         n = n // 2
+    return binaryno
 
-DecimalToBinaryMethod1(n)
+print(DecimalToBinaryMethod1(n))

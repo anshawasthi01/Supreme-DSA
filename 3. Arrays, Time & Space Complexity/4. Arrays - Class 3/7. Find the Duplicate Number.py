4b2737b2270992c9317ad2e25fa29c2294891d65
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-17 19:49:57 +0530

    Home Assignment Leetcode

---
+++ 3. Arrays, Time & Space Complexity/4. Arrays - Class 3/7. Find the Duplicate Number.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};

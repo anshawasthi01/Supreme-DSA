466b99f521a78bd10c0f3e0a49c9b22e2aee7d39
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-17 19:15:06 +0530

    Home Assignment

---
+++ 3. Arrays, Time & Space Complexity/4. Arrays - Class 3/5. Sort 0's 1's and 2's.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+
+        first, cur, last  = 0, 0, len(nums)-1
+ 
+        while(cur<=last):
+            if nums[cur] == 0:
+                nums[first], nums[cur] = nums[cur], nums[first]
+                cur+=1
+                first+=1
+            
+            elif nums[cur] == 1:
+                cur+=1
+                
+            else:
+                nums[cur], nums[last] = nums[last], nums[cur]
+                last-=1
+        return nums

5d7dcee6769f35ac20d66a96b5ddd6677dc28341
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-18 15:14:57 +0530

    Home Assignment gfg set

---
+++ 3. Arrays, Time & Space Complexity/4. Arrays - Class 3/10. Common Elements in 3 Sorted Array.py
@@ -0,0 +1,55 @@
+# https://practice.geeksforgeeks.org/problems/common-elements1132/1
+
+        ans = []
+        st = set()
+        i, j, k = 0, 0, 0
+        while i<n1 and j<n2 and k<n3:
+            if A[i] == B[j] and B[j] == C[k]:
+                st.add(A[i])
+                i -= 1
+                j -= 1
+                k -= 1
+            elif A[i]<B[j]:
+                i += 1
+            elif B[j]<C[k]:
+                j += 1
+            else:
+                k += 1
+                
+        for i in (st):
+            ans.append(i)
+            
+        return ans
+
+
+
+# class Solution
+# {
+#     public:    
+#        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+#         {
+#             vector<int>res;
+#             set<int>st;
+#             int i,j,k;
+#             i=j=k=0;
+#             while(i<n1 && j<n2 && k<n3){
+#                 if (A[i] == B[j] && B[j] == C[k]){
+#                     st.insert(A[i]);
+#                     i++,j++,k++;
+#                 }
+#                 else if(A[i]<B[j]){
+#                     i++;
+#                 }
+#                 else if(B[j]<C[k]){
+#                     j++;
+#                 }
+#                 else{
+#                     k++;
+#                 }
+#             }
+#             for(auto i:st){
+#                 res.push_back(i); 
+#             }
+#         return res;
+#         }
+# };

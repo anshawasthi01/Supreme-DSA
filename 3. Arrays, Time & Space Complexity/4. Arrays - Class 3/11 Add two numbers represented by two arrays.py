4f55178b8cca7c6f5470825f2d1af4c379b5c355
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-18 16:02:36 +0530

    Home Assignment gfg Add two numbers represented by two arrays

---
+++ 3. Arrays, Time & Space Complexity/4. Arrays - Class 3/11 Add two numbers represented by two arrays.py
@@ -0,0 +1,50 @@
+# https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-two-arrays2408/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+# https://www.geeksforgeeks.org/add-two-numbers-represented-by-two-arrays/
+
+class Solution:
+    def calc_Sum (self, a,  n, b, m) :
+        #Complete the function
+        f = ""
+        s = ""  # a.push_back(digit+ '0')
+        for i in arr:
+            f += str(i)
+        for i in brr:
+            s += str(i)
+        return int(f) + int(s)
+        
+        
+def helperAdd(a, b, n, m) :
+    carry = 0
+    ans = ''
+    i = n-1
+    j = m-1
+        
+    while i >= 0 and j >= 0:
+        x = a[i] + b[j] + carry
+        ans += str(x % 10)
+        carry = x // 10  # In cpp /
+        i -= 1
+        j -= 1
+            
+    while i >= 0:
+        x = a[i] + carry
+        ans += str(x % 10)
+        carry = x // 10     # In cpp /
+        i -= 1
+            
+    while j >= 0:
+        x = b[j] + carry
+        ans += str(x % 10)
+        carry = x // 10    # In cpp /
+        j -= 1
+    
+    if carry == 1:
+        ans += str(carry)
+    return int(ans[::-1])
+
+class Solution:
+    # def calc_Sum (self, a,  n, b, m) :
+    def calc_Sum (self, a1,  n1, a2, n2): 
+        if n1 < n2:
+            return helperAdd(a2,a1,n2,n1)
+        return helperAdd(a1,a2,n1,n2)

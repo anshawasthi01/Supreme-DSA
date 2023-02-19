45e06f407ebeae0a8b728a095a8ada0321b8da8e
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-19 18:42:21 +0530

    Peak element leetcode

---
+++ 4. Searching & Sorting/1. Searching & Sorting Class - I/5. Peak Index in a Mountain Array.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+# CodeHelp
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        s, e = 0, len(arr)-1
+
+        while s < e:
+            mid = s + (e - s)//2
+            if arr[mid] < arr[mid+1]:
+                s = mid + 1
+            else:
+                e = mid
+        
+        return e
+        return s

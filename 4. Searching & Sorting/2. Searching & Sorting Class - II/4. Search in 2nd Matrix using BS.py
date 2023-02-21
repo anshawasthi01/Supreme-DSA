5792e1ab2200fd594fbb549d34df16209dfd5cd8
anshawasthi01 <anshawasthi3894@gmail.com>
2023-02-21 20:15:05 +0530

    leetcode

--- 4. Searching & Sorting/2. Searching & Sorting Class - II/4. Search in 2nd Matrix using BS.py
+++ 4. Searching & Sorting/2. Searching & Sorting Class - II/4. Search in 2nd Matrix using BS.py
@@ -30,6 +30,45 @@ else:
     print("Not Found ") 
 
 
+# class Solution:
+#     def searchMatrix(self, arr: List[List[int]], target: int) -> bool:
+
+#         rows = len(arr)
+#         cols = len(arr[0])
+#         s, e = 0, rows*cols - 1
+#         mid = s + (e-s)//2
+
+#         while s <= e:
+#             rowIndex = mid//cols
+#             colIndex = mid%cols
+    
+#             if arr[rowIndex][colIndex] == target:
+#                 print("Found at ", rowIndex, " ", colIndex)
+#                 return True
+    
+#             if arr[rowIndex][colIndex] < target:
+#                 s = mid +1
+
+#             else:
+#                 e = mid - 1
+#             mid = s + (e-s)//2
+#         return False
+#         --------------
+#         n = len(matrix)
+#         m = len(matrix[0])
+#         i = 0
+#         j = m-1
+        
+#         while i!=n and j!=-1:
+#             if matrix[i][j]>target:
+#                 j-=1
+#             elif matrix[i][j]<target:
+#                 i+=1
+#             else:
+#                 return True
+#         return False
+
+
 
 
 

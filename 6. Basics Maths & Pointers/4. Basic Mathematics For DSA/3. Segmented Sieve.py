109c505801a73d7c330af9033790d541c7636387
anshawasthi01 <anshawasthi3894@gmail.com>
2023-03-19 01:31:03 +0530

    Segmented Sieve

--- 6. Basics Maths & Pointers/4. Basic Mathematics For DSA/3. Segmented Sieve.py
+++ 6. Basics Maths & Pointers/4. Basic Mathematics For DSA/3. Segmented Sieve.py
@@ -3,11 +3,11 @@ def Sieve(n):
     if(n<=1):
         return 0
 
-    sieve = [True] * n
+    sieve = [True] * (n+1)
     sieve[0] = sieve[1] = False
 
-    for i in range(2,n):
-        if i*i < n: # Optimization 2
+    for i in range(2,n+1):
+        if i*i <= n: # Optimization 2
             if sieve[i] == True:
                 j = i*i # Optimization 1 : first unmarked number would be i*i,, as others have been marked by 2 to (i - 1)
                 while(j < n):

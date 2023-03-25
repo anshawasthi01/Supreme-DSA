079c93f64d459c251e276d44fb59350d551f5284
anshawasthi01 <anshawasthi3894@gmail.com>
2023-03-25 22:15:44 +0530

    N Queen

--- 8. Divide & Conquer, Time Complexity & Space Complexity/4. DnC Level - 4 [BackTracking]/1. N Queen.py
+++ 8. Divide & Conquer, Time Complexity & Space Complexity/4. DnC Level - 4 [BackTracking]/1. N Queen.py
@@ -1,93 +1,75 @@
-#include <iostream>
-using namespace std;
-#include <vector>
+def printSolution(board, n):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=" ")
+        print()
+    print("\n")
 
 
-void printSolution(vector<vector<char>> &board, int n) {
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n ;j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl << endl;
-}
+def isSafe(row, col, board, n):
+    # check karna chahte h , k kya main 
+    # current cell [row,col] pr    QUEEN rakh 
+    # sakta hu ya nahi
+    # check if it is safe to place a queen at current cell (row, col)
+    i, j = row, col
+
+    # check row
+    while j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        j -= 1
+
+    # check upper left diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+
+    # check bottom left diagonal
+    i, j = row, col
+    while i < n and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i += 1
+        j -= 1
+
+    # kahin pr bhi queen nahi mili
+    # iska matlab ye position safe hai 
+    # iska matlab eturn kardo true
+    # if no queen found in the same row or the two diagonals,
+    # it is safe to place a queen at current cell
+    return True
 
 
-bool isSafe(int row, int col, vector<vector<char>> &board, int n) {
-  // check karna chahte h , k kya main 
-  // current cell [row,col] pr    QUEEN rakh 
-  // sakta hu ya nahi
-  int i = row;
-  int j = col;
+def solve(board, col, n):
+    # base case
+    if col >= n:
+        printSolution(board, n)
+        return
 
-  //check row
-  while(j >= 0) {
-    if(board[i][j] == 'Q') {
-      return false;
-    }
-    j--;
-  }
+    # 1 case solve karna h , baaki recursion sambhal lega
+    # try to place a queen in each row of the current column
+    for row in range(n):
+        if isSafe(row, col, board, n):
+            # rkh do
+            # place a queen at (row, col)
+            board[row][col] = 'Q'
 
-  //check upper left diaglnol 
-  i = row;
-  j = col;
-  while(i>=0 && j>=0 ) {
-    if(board[i][j] == 'Q'){
-      return false;
-    }
-    i--;
-    j--;
-  }
+            # recursion soluion lgega
+            # try to place queens in the next columns
+            solve(board, col + 1, n)
+
+            # backtracking
+            # backtrack and remove the queen from (row, col)
+            board[row][col] = '-'
 
 
-  //check bottom left diagnol
-  i = row;
-  j = col;
-  while( i < n && j >=0) {
-    if(board[i][j] == 'Q') {
-      return false;
-    }
-    i++;
-    j--;
-  }
 
-  //kahin pr bhi queen nahi mili
-  //iska matlab ye position safe hai 
-  //iska matlab eturn kardo true
-  return true;   
-}
-
-
-void solve(vector<vector<char>> &board, int col, int n) {
-  //base case
-  if(col >= n) {
-    printSolution(board, n);
-    return ;
-}
-
-//1 case solve karna h , baaki recursion sambhal lega
-
-for(int row = 0; row <n; row++) {
-    if(isSafe(row, col, board, n)) {
-    //rakh do
-    board[row][col] = 'Q';
-            
-    //recursion solution laega
-    solve(board, col+1, n);
-    //backtracking
-    board[row][col] = '-';
-    }
-  }
-}
-
-
-int main(){
-  int n = 4;
-  vector<vector<char>> board(n, vector<char>(n,'-'));
-  int col = 0;
-  //0 -> empty cell
-  //1 -> Queen at the cell
-  solve(board, col, n);
-  return 0;
-};
+n = 4
+board = [['-' for j in range(n)] for i in range(n)]
+col = 0
+# 0 -> empty cell
+# 1 -> Queen at the cell
+solve(board, col, n)

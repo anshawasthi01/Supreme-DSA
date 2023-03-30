27ee7fc858e1e1fbef72183945c07b70ab1de158
anshawasthi01 <anshawasthi3894@gmail.com>
2023-03-31 00:10:32 +0530

    Class Code

--- 8. Divide & Conquer, Time Complexity & Space Complexity/6. DnC Level - 6[BackTracking]/main (13)  Sudoku Solver.cpp
+++ 8. Divide & Conquer, Time Complexity & Space Complexity/6. DnC Level - 6[BackTracking]/main (13)  Sudoku Solver.cpp
@@ -14,6 +14,8 @@ public:
 
             //3*3 box check
             if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == value) 
+            // if(board[      3*(row/3)          +   (i/3)    ][3*(col/3)+(i%3)] == value) 
+            //          starting row of a boxes     movement
                 return false;
         }
         return true;

51ba8739a961af76aeca06b83817d644bfb13466
anshawasthi01 <anshawasthi3894@gmail.com>
2023-04-01 18:00:06 +0530

    Recursion

---
+++ 7. Recursion and Backtracking/6. Assignements/7. Best Time to Buy and Sell Stock [Leetcode121].py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+# CodeHelp
+
+def maxProfitFinder(prices, i, minPrice, maxProfit):
+    if i == len(prices): return
+
+    if prices[i] < minPrice[0]: minPrice[0] = prices[i]
+    todayProfit = prices[i] - minPrice[0]
+    if todayProfit > maxProfit[0]: maxProfit[0] = todayProfit
+
+    # RE
+    maxProfitFinder(prices, i+1, minPrice, maxProfit)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # # Iterative
+        # maxprofit=0
+        # minpricetillnow=9999999999
+        # for price in prices:
+        #     maxprofit=max(maxprofit,price-minpricetillnow)
+        #     minpricetillnow=min(minpricetillnow, price)
+        # return maxprofit
+        minPrice = [9999999]
+        maxProfit = [0]
+        maxProfitFinder(prices, 0, minPrice, maxProfit)
+        return maxProfit[0]
+
+        

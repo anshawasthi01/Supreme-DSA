099ec4c46a0ac65a1eb0c2fdf679f79496bbc9c3
anshawasthi01 <anshawasthi3894@gmail.com>
2023-04-23 17:53:01 +0530

    check cycle detetct starting point and remove cycle

---
+++ 10. Linked Lists/3. LL Class - III/3. Check, DetectPoint and Remove Loop.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+def printList(head):
+    temp = head
+    while temp:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print()
+
+
+def checkForLoop(head):
+    if head is None:
+        print("LL is empty")
+        return False
+
+    slow = head
+    fast = head
+
+    while fast != None:
+        fast = fast.next
+        if fast != None:
+            fast = fast.next
+            slow = slow.next
+
+        if slow == fast:
+            # loop present
+            return True
+
+    # fast None hogya
+    return False
+
+
+def startingPointLoop(head):
+    if not head:
+        print("LL is empty")
+        return None
+
+    slow = head
+    fast = head
+
+    while fast:
+        fast = fast.next
+        if fast:
+            fast = fast.next
+            slow = slow.next
+
+        if slow == fast:
+            slow = head
+            break
+
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+
+    return slow
+
+
+def removeLoop(head):
+    if not head:
+        print("LL is empty")
+        return None
+
+    slow = head
+    fast = head
+
+    while fast:
+        fast = fast.next
+        if fast:
+            fast = fast.next
+            slow = slow.next
+
+        if slow == fast:
+            slow = head
+            break
+
+    prev = fast
+    while slow != fast:
+        prev = fast
+        slow = slow.next
+        fast = fast.next
+    prev.next = None
+    return slow
+
+
+
+head = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(3)))))))
+head.next.next.next.next.next.next = head.next.next.next
+# printList(head)
+print("Loop is Present or not: ", checkForLoop(head))
+startPointLoop = startingPointLoop(head)
+print("Starting Point of LOOP is: ", startPointLoop.data)
+removeLoop(head)
+printList(head)
+print("Loop is Present or not: ", checkForLoop(head))

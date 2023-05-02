95796854aa593f257939fae478e95e27be821d3e
anshawasthi01 <anshawasthi3894@gmail.com>
2023-05-02 14:57:25 +0530

    Min Stack L-155 Using pair

---
+++ 11. Stack/3. Stack/1. Min Stack [L-155].py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/min-stack/description/
+
+# CodeHelp
+
+class MinStack:
+    def __init__(self):
+        self.st = []
+
+    def push(self, val: int) -> None:
+        if not self.st:
+            self.st.append((val, val))
+        else:
+            self.st.append((val, min(val, self.st[-1][1])))
+
+    def pop(self) -> None:
+        self.st.pop()
+
+    def top(self) -> int:
+        return self.st[-1][0]
+
+    def getMin(self) -> int:
+        return self.st[-1][1]

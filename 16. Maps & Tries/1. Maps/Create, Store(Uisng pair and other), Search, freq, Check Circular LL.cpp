b62ff91ed59e92c77866fdeba401420298d73ec3
anshawasthi01 <anshawasthi3894@gmail.com>
2023-05-29 22:04:22 +0530

    Class Code

---
+++ 16. Maps & Tries/1. Maps/Create, Store(Uisng pair and other), Search, freq, Check Circular LL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+  // creation
+  unordered_map<string, int> m;
+
+  // insertion
+  pair<string, int> p = make_pair("Scorpio", 9);
+  m.insert(p);
+  pair<string, int> p2("alto", 2);
+  m.insert(p2);
+
+  m["fortuner"] = 10; // Using this method in not in map then create entry and initialize with 0
+
+  // access
+  cout << m.at("alto") << endl;
+  cout << m.at("Scorpio") << endl;
+
+  // string str = "thfdsiuejkfsdiuesd"
+  // unordered_map<char, int> freq;
+
+  // for(int i=0; i<str.length(); i++){
+  //   char ch = str[i];
+  //   freq[ch]++;
+  // }
+
+  // for(auto i: freq){
+  //   cout << i.first << " " << i.second << endl;
+  // }
+  return 0;
+}
+
+
+// bool checkCircular(Node* head){
+//   unordered_map<node*, bool> vis;
+//   Node* temp = head;
+
+//   while(temp!=NULL){
+//     if(vis.find(temp) != vis.end()){
+//       vis[temp] = temp;
+//     }
+//     else{
+//       return true;
+//     }
+//     temp = temp -> next;
+//   }
+//   return false;
+// }

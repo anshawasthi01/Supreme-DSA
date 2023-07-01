5763a0fc1bc40dadaa6cf44463103cb42f9c7c9c
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-01 12:09:07 +0530

    Class Code

---
+++ 20. Graph - II/2. Graph/classcode/1. Course Schedule.cpp
@@ -0,0 +1,63 @@
+class Solution {
+private:
+	bool topoSortBfs(int n, unordered_map<int,list<int> >& adjList) {
+		vector<int> ans;
+		queue<int> q;
+		unordered_map<int, int> indegree;
+
+		//indegree calculate
+		for(auto i: adjList ) {
+			int src = i.first;
+			for(auto nbr:i.second) {
+				indegree[nbr]++;
+			}
+		}
+
+		//put all nodes inside queue, which has indegree=0
+		for(int i=0; i<n; i++) {
+			if(indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+
+		//bfs logic
+		while(!q.empty()) {
+			int fNode = q.front();
+			q.pop();
+
+			ans.push_back(fNode);
+			//or we can do count++
+
+			for(auto nbr: adjList[fNode]) {
+				indegree[nbr]--;
+				//check for zero again
+				if(indegree[nbr] == 0) {
+					q.push(nbr);
+				}	
+			}
+			
+		}
+
+		if(ans.size() == n) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,list<int> > adjList;
+
+				for(auto task: prerequisites) {
+					int u = task[0];
+					int v = task[1];
+					adjList[v].push_back(u);
+				}
+
+				bool ans = topoSortBfs(numCourses, adjList);
+				return ans;
+    }
+};

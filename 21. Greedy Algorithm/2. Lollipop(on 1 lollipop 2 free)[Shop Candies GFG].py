51e11b30131e9cacd132a2cb8914df31fa4fe123
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-08 17:21:55 +0530

    Short Candies

---
+++ 21. Greedy Algorithm/2. Lollipop(on 1 lollipop 2 free)[Shop Candies GFG].py
@@ -0,0 +1,17 @@
+prices = [2, 8, 6, 9, 4, 7]
+n = 6
+
+# algo starts here
+prices.sort()
+
+amount = 0
+buy = 0
+free = n - 1
+
+while buy <= free:
+    amount += prices[buy]
+    buy += 1
+    free -= 1
+    free -= 1
+
+print("Answer is", amount)
